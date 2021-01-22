728efb0db75898079ec27e716c484dfd86c31d60
luklus <ll_code@outlook.com>
2021-01-22 10:20:22 +0100

    feat: add footer & header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { AppRoutingModule } from './app-routing.module'
 import { BrowserModule } from '@angular/platform-browser'
 import { HomeModule } from './modules/home/home.module'
 import { NgModule } from '@angular/core'
-import { StoreModule } from '@ngrx/store'
+import { StoreModule } from '@ngrx/store';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component'
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
   imports: [
     AppRoutingModule,
     BrowserModule,
